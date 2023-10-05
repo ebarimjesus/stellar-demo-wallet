b2c8ec31809445a093f9c69cc399b5bfad6a8cf2
ebarimjesus <130609758+ebarimjesus@users.noreply.github.com>
2023-10-05 12:49:53 +0000

    update

--- packages/demo-wallet-client/public/settings/env-config.js
+++ packages/demo-wallet-client/public/settings/env-config.js
@@ -3,8 +3,9 @@ window._env_ = {
   SENTRY_API_KEY: "",
   HORIZON_PASSPHRASE: "Public Global Stellar Network ; September 2015",
   HORIZON_URL: "https://horizon.stellar.org",
-  CLIENT_DOMAIN: "https://zingypay.com",
+  CLIENT_DOMAIN: "wallet.zingypay.com",
   WALLET_BACKEND_ENDPOINT: "https://wallet.zingypay.com",
+  SIGNING_KEY: "SAJE2GCLEETVNJI5UHNU5ED5EKAFB7ORAKDGP6HDIHSIYOX7A46FCIDG",
 };
 
 
